b509bb8b82fae40142d8a4feb2279bf39ac58f2e
Calle Robertsson <calle@upset.se>
2017-09-23 17:55:52 +0200

    Add delta comparision for operator tests

--- rpncalc/operators_test.go
+++ rpncalc/operators_test.go
@@ -1,6 +1,9 @@
 package rpncalc
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestUnaryOp(t *testing.T) {
 
@@ -39,7 +42,13 @@ func TestBinaryOp(t *testing.T) {
 		err error
 	}{
 		{opAddition, 1, -1, 0, nil},
+		{opAddition, 3.1415, 2.0, 5.1415, nil},
 		{opAddition, 123456789.12345, -1, 123456788.12345, nil},
+		{opSubtraction, 3.1415, 2.0, 1.1415, nil},
+		{opSubtraction, 3.1415, -2.0, 5.1415, nil},
+		{opSubtraction, 123456789.12345, -1, 123456790.12345, nil},
+		{opMultiplication, 101.0, 10.0, 1010.0, nil},
+		{opMultiplication, 1.23, -100.0, -123.0, nil},
 		{opDivision, 101.0, 10.0, 10.1, nil},
 		{opDivision, 101.0, 0.0, 0.0, errDivisionByZero},
 
@@ -53,9 +62,15 @@ func TestBinaryOp(t *testing.T) {
 			t.Errorf("Case %d: Expected error %q, but got %q", i, c.err, err)
 		}
 
-		if got != c.exp {
+		if !almostEqual(got, c.exp) {
 			t.Errorf("Case %d: Expected result %v, but got %v", i, c.exp, got)
 		}
 	}
 
+}
+
+func almostEqual(x, y float64) bool {
+	delta := 0.000000000000001
+	//fmt.Printf("diff %v, delta %v\n", math.Abs(x-y), delta)
+	return math.Abs(x-y) < delta
 }
